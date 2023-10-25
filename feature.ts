5b730e76ffd2e2f350f429e0c4e6ff9d8c9e1577
nadian-melhem <60795209+nadian-melhem@users.noreply.github.com>
2023-10-25 18:45:40 +0300

    editing feature.ts file

--- feature.ts
+++ feature.ts
@@ -1,4 +1,4 @@
-// Calculate the sum of two numbers edit this line
+// Calculate the sum of two numbers this fle was editied
 function calculateSum(a: number, b: number): number {
     return a + b;
 }
